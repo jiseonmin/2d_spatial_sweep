a117b0172d16f2bd8587e8f17c104a2e09c30758
jiseonmin <minjiseon0827@gmail.com>
2025-09-20 13:44:11 -0400

    change parameters, add post slim process, right now commented out slim sim part, debugging the python script

--- test_run.sh
+++ test_run.sh
@@ -4,17 +4,20 @@
 #SBATCH -o output_%j.out                    
 #SBATCH -e error_%j.err                   
 #SBATCH --job-name=test
-#SBATCH --mem=100G
+#SBATCH --mem=200G
 #SBATCH --partition=short
 
 module load anaconda3/2024.06
 eval "$(conda shell.bash hook)"
 conda activate 2d-sweep
 
-L1=500
-L2=500
-rho=500
+L1=100
+L2=100
+rho=100
 m=0.25
-s=0.05
+s=0.005
 
-slim -d L1=$L1 -d L2=$L2 -d rho=$rho -d m=$m -d s=$s 2d_spatial_sweep.slim
+#slim -d L1=$L1 -d L2=$L2 -d rho=$rho -d m=$m -d s=$s 2d_spatial_sweep.slim
+#echo "slim sim finished"
+python post_slim_process.py
+echo "finished pyslim + msprime post-processing"
