20dd8276b5fe229577feb2f0be99b68709d35e45
jiseonmin <minjiseon0827@gmail.com>
2025-09-23 11:55:18 -0400

    save intermediate ts's and print time to check how long each step takes

--- post_slim_process.py
+++ post_slim_process.py
@@ -3,11 +3,14 @@ import pyslim
 import msprime
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
+
 
 ts = tskit.load("fixed_sweep.trees")
-
+print("ts loaded at " + datetime.now())
 # SLiM apparently creates an older version of ts than what pyslim uses. So I will update it.
 ts = pyslim.update(ts)
+print("ts updated at " + datetime.now())
 
 metadata = ts.metadata['SLiM']['user_metadata']
 L1 = metadata['L1'][0]
@@ -30,19 +33,23 @@ for i in range(L1):
         if j<L2-1:
             demography.set_migration_rate(home, home+L1, m)
 
-
+print("start recapitating at " + datetime.now())
 
 rts = pyslim.recapitate(
         ts, demography=demography,
         recombination_rate=1e-8,
         random_seed=4
 )
+print("recapitation completed. Adding neutral mutations at " + datetime.now())
+
 # Sprinkle neutral mutations on top of the recapitated tree sequence
 next_id = pyslim.next_slim_mutation_id(rts)
 rts = msprime.sim_mutations(
             rts, rate=1e-8, random_seed=7, keep=True,
             model=msprime.SLiMMutationModel(type=0, next_id=next_id)
 )
+rts.dump("recapitated_ts.trees")
+print("neutral mutations added. Start running forward-in-time sims post SLiM at " + datetime.now())
 
 # run forward-in-time sim after sweep
 new_time = 1000
@@ -72,16 +79,22 @@ new_ts = msprime.sim_ancestry(
               sequence_length=rts.sequence_length,
               recombination_rate=r,
               random_seed=9)
+new_ts.dump("post_slim_ts.trees")
+print("msprime sim ancestry done. now adding mutations to the new ts at " + datetime.now())
+
 new_ts = msprime.sim_mutations(
                  new_ts, rate=1e-8, random_seed=10, keep=True,
                  model=msprime.SLiMMutationModel(type=0)
         )
+
 new_tables = new_ts.tables
 
 new_nodes = np.where(new_tables.nodes.time == new_time)[0]
-print(f"There are {len(new_nodes)} nodes from the start of the new simulation.")
+print("There are {len(new_nodes)} nodes from the start of the new simulation. Current time: " + datetime.now())
 
 slim_nodes = rts.samples(time=0)
+
+print("nodes sampled from the original ts at " + datetime.now())
 
 # randomly give new_nodes IDs in rts
 node_map = np.repeat(tskit.NULL, new_tables.nodes.num_rows)
@@ -95,6 +108,7 @@ tables.nodes.flags = tables.nodes.flags & ~np.uint32(tskit.NODE_IS_SAMPLE)
 tables.nodes.time = tables.nodes.time + new_time
 tables.mutations.time = tables.mutations.time + new_time
 
+print("merging tables at " + datetime.now())
 # merge the two sets of tables
 tables.union(new_tables, node_map,
             add_populations=False,
@@ -102,6 +116,7 @@ tables.union(new_tables, node_map,
 
 # get back the tree sequence
 full_ts = tables.tree_sequence()
+full_ts.dump("full_ts.trees")
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 afs = ts.allele_frequency_spectrum(polarised=True, mode='branch')
